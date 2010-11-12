e36570924b63b15459d1d5fbaf51c22f164068d0
Damien Langg <damien.langg@gmail.com>
2010-11-12 12:53:30 +0000

    makerel update

--- makerel.sh
+++ makerel.sh
@@ -7,28 +7,14 @@ ZIP=$RELDIR-$VER.zip
 
 rm -rf $RELDIR $ZIP
 
-mkdir $RELDIR
-mkdir $RELDIR/lib
-mkdir $RELDIR/doc
-mkdir $RELDIR/demo
+svn export . $RELDIR
 
-cp moviecat.pl interactive.cmd run_scan.cmd $RELDIR
-
-cp moviecat.js IMDB_Movie.pm *.css  $RELDIR/lib
-
-if [ -e default-cfg.txt ]; then
-    cp default-cfg.txt $RELDIR/config.txt
-else
-    cp config.txt $RELDIR/config.txt
-fi
-cp readme.txt $RELDIR
-cp license.txt gpl.txt todo.txt sample-cfg.txt changelog.txt  $RELDIR/doc
-
-tar cf - demo --exclude CVS --exclude '*.jpg' | tar xvf - -C $RELDIR
-cp demo.cmd demo-cfg.txt $RELDIR/demo
+rm $RELDIR/makerel.sh
 
 unix2dos $RELDIR/*.txt $RELDIR/doc/*.txt $RELDIR/demo/*.txt
 unix2dos $RELDIR/*.cmd $RELDIR/demo/*.cmd
 
 zip -r $ZIP $RELDIR
+
+mv $RELDIR $RELDIR-$VER
 
